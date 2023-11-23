148d66a230c148edbc2374f26b555cf8fbf1c238
Prasad <prasadmhaske2001@gmail.com>
2023-11-23 18:27:51 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,48 @@
-import './App.css';
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
+const imageData = () => {
   let data = [
     {
-      id:1,
-      img:elephant
+      id: 1,
+      img: elephant,
     },
     {
-      id:2,
-      img:elephant
+      id: 2,
+      img: elephant,
     },
     {
-      id:3,
-      img:elephant
+      id: 3,
+      img: elephant,
     },
     {
-      id:4,
-      img:elephant
-    }
-  ]
+      id: 4,
+      img: elephant,
+    },
+  ];
   return data;
-}
+};
 
 function App() {
   // code here
+  let data = imageData();
+  // console.log(data)
+
+  return (
+    <>
+      <h1 className="heading">Kalvium gallary</h1>
+      <div className="container">
+        {data.map((ele) => {
+          return (
+            <div key={ele.id}>
+              <img src={ele.img} alt="elephant" />
+              <p>{ele.id}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -1,30 +1,48 @@
 import { Component } from "react";
-import "./App.css"
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-export default class AppClass extends Component{
-  
-  imageData = ()=>{
+export default class AppClass extends Component {
+  imageData = () => {
     let data = [
       {
-        id:1,
-        img:elephant
+        id: 1,
+        img: elephant,
       },
       {
-        id:2,
-        img:elephant
+        id: 2,
+        img: elephant,
       },
       {
-        id:3,
-        img:elephant
+        id: 3,
+        img: elephant,
       },
       {
-        id:4,
-        img:elephant
-      }
-    ]
+        id: 4,
+        img: elephant,
+      },
+    ];
     return data;
-  }
+  };
 
   // code here
+  render() {
+    const data = this.imageData();
+
+    return (
+      <>
+        <h1 className="heading">Kalvium gallary</h1>
+        <div className="container">
+          {data.map((ele) => {
+            return (
+              <div key={ele.id}>
+                <img src={ele.img} alt={"elephant"} />
+                <p>{ele.id}</p>
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
+  }
 }
